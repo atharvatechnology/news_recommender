62878e84fa0c56ab9e32e6d2b0a653fdd45aa377
SaRu-N <saruniroula73@gmail.com>
2023-02-09 15:33:01 +0545

    create tag model

--- movierec/dataset/models.py
+++ movierec/dataset/models.py
@@ -9,25 +9,25 @@ class Movie(models.Model):
     def __str__(self):
         return self.title
 
-# class Tag(models.Model):
-#     userId=models.IntegerField()
-#     movieId=models.ManyToManyField(Movie,related_name='tags')
-#     tag=models.CharField(max_length=500)
-#     timestamp=models.DateTimeField(auto_now_add= True)
+class Tag(models.Model):
+    userId=models.IntegerField()
+    movieId=models.ManyToManyField(Movie,related_name='tags')
+    tag=models.CharField(max_length=500)
+    timestamp=models.DateTimeField(auto_now_add= True)
 
-#     def __str__(self):
-#         return self.tag
+    def __str__(self):
+        return self.tag
 
-# class Link(models.Model):
-#     movieId=models.ForeignKey(Movie,related_name='links',on_delete=models.CASCADE)
-#     imdbId=models.IntegerField()
-#     tmdbId=models.IntegerField()
+class Link(models.Model):
+    movieId=models.ForeignKey(Movie,related_name='links',on_delete=models.CASCADE)
+    imdbId=models.IntegerField()
+    tmdbId=models.CharField(max_length=100)
 
-# class Rating(models.Model):
-#     userId=models.IntegerField()
-#     movieId=models.ForeignKey(Movie,related_name='ratings',on_delete=models.CASCADE)
-#     rating=models.FloatField()
-#     timestamp=models.DateTimeField(auto_now_add= True)
+class Rating(models.Model):
+    userId=models.IntegerField()
+    movieId=models.ForeignKey(Movie,related_name='ratings',on_delete=models.CASCADE)
+    rating=models.FloatField()
+    timestamp=models.DateTimeField(auto_now_add= True)
 
 
 
