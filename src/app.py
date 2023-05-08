2add6b876a766cf5caac62d6590842ba105cfaac
shovan777 <raj.shrestha778@gmail.com>
2023-05-08 14:22:12 +0545

    Respond with array of news_ids on rec request.

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
+import json
 from os import path
-from flask import Flask
+from flask import Flask, request
 import pandas as pd
 import torch
 from .evaluate import (
@@ -10,12 +11,15 @@ app = Flask(__name__)
 
 
 # specify response type as json
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["POST"])
 def hello():
+    payload = request.get_json()
+    print(f"*****payload: {payload}*****")
     news_df = pd.read_csv(path.join("../data", "news_processed.csv"))
     ratings_df = pd.read_csv(path.join("../data", "ratings_processed.csv"))
     embeddings = torch.load(path.join("../data", "embeddings.pt"))["embedding_dict"]
     # print(news_df.head())
     recs = user_recommendations(embeddings, news_df, ratings_df, user_id=7170)
-    print(recs.to_json())
-    return recs.to_json()
+    rec_dict = {"recs": recs}
+
+    return json.dumps(rec_dict)
