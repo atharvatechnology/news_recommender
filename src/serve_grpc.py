a6f2b9e21520c1fefbe35eda0b503ddb630c044d
shovan777 <raj.shrestha778@gmail.com>
2023-06-06 14:56:35 +0545

    Retrain model.

--- src/serve_grpc.py
+++ src/serve_grpc.py
@@ -17,19 +17,20 @@ from evaluate import user_recommendations
 class Recommender(NewsRecommendationServiceServicer):
     def __init__(self):
         print("Loading data...")
-        # self.news_df = pd.read_csv(path.join("data", "news_processed.csv"))
-        # self.ratings_df = pd.read_csv(path.join("data", "ratings_processed.csv"))
-        # self.embeddings = torch.load(path.join("data", "embeddings.pt"))[
-        #     "embedding_dict"
-        # ]
+        self.news_df = pd.read_csv(path.join("data", "news_processed.csv"))
+        self.ratings_df = pd.read_csv(path.join("data", "ratings_processed.csv"))
+        self.embeddings = torch.load(path.join("data", "embeddings.pt"))[
+            "embedding_dict"
+        ]
 
     def GetNewsRecommendation(self, request: GetNewsRecommendationRequest, context):
         user_id = request.userId
-        # recs = user_recommendations(
-        #     self.embeddings, self.news_df, self.ratings_df, user_id=user_id
-        # )
-        # TODO: Delete this line later
-        recs = [90, 91]
+        print(f"*****Received request for user {user_id}")
+        recs = user_recommendations(
+            self.embeddings, self.news_df, self.ratings_df, user_id=user_id, k=20
+        )
+        # # TODO: Delete this line later
+        recs = [i for i in range(1, 21)]
         return GetNewsRecommendationResponse(newsIds=recs)
 
 
